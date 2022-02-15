d560177f6f5f848982961f8f6a0bc5d331def351
ota.seiji <ota.seiji@classmethod.jp>
2022-02-15 18:39:49 +0900

    不要なコメントを削除

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,18 +5,15 @@ const app = express()
 
 const prismaClient = new prisma.PrismaClient()
 
-// body-parserに基づいた着信リクエストの解析
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// Get
 app.get('/users/:id', async (req: express.Request, res: express.Response) => {
   const id = req.params.id
   const user = await prismaClient.user.findUnique({ where: { id: Number(id) } })
   res.send(user ?? `User not found`)
 })
 
-// POST
 app.post('/users', async (req: express.Request, res: express.Response) => {
   const name = req.body.name
   const user = await prismaClient.user.create({
@@ -27,7 +24,6 @@ app.post('/users', async (req: express.Request, res: express.Response) => {
   res.send(user)
 })
 
-// 3000番ポートでAPIサーバ起動
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
 })
